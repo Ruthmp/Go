999de5e67860560aaaea82bfa5a6a8a6b1dac260
Ruthmp <mpruth3@gmail.com>
2025-03-19 07:31:19 +0100

    Ejercicio 2

---
+++ Ejercicio_2.go
@@ -0,0 +1,24 @@
+//2. Programa que acepte un argumento CLI e imprima "Hola, [argumento]".
+
+package main
+
+import "fmt"
+
+func main() {
+
+	//Ejercicio 2
+
+	var myVar string = "Go"
+	fmt.Println("Hola" + " " + myVar)
+
+	// Ejercicio 3
+
+	num1 := 5
+	num2 := 3
+
+	suma := num1 + num2
+
+	fmt.Println("La suma de ambos números es", suma)
+
+	//Ejercicio 4
+}
